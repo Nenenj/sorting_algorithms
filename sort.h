c50ee6ffbc961151b3ee6ec7526a35be7953818e
Nenenj <nnennanjoku08@gmail.com>
2023-03-09 01:40:18 -0800

    sort.h

--- sort.h
+++ sort.h
@@ -40,7 +40,7 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-int partition(int *array, int lo, int hi, size_t size);
+int  partition(int *array, int lo, int hi, size_t size);
 void quick_s(int *array, int lo, int hi, size_t size);
 void merge(size_t lo, size_t mi, size_t hi, int *dest, int *src);
 void merge_partition(size_t lo, size_t hi, int *array, int *base);
